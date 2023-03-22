5abd30344357262d2b1306bea62aada2beeab968
Ali <smalihaiderr@gmail.com>
2023-03-23 01:49:55 +0500

    As of yet.

--- src/App.js
+++ src/App.js
@@ -1,114 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
-import {AppShell, Navbar, MediaQuery, Text, Header, Burger,ActionIcon, Group} from '@mantine/core'
-import { MantineProvider } from '@mantine/core';
-import {SunIcon, MoonIcon} from '@modulz/radix-icons'
-import {useState} from 'react'
-import { createStyles, GlobalStyles, useMantineTheme } from '@mantine/styles';
-import {MemoryRouter,NavLink,Route,Routes} from 'react-router-dom'
-import {invoke} from '@tauri-apps/api/tauri'
+import { useState } from 'react';
+import Login from './Login.jsx';
+import Home from './Home.js';
+import Register from './Register';
+import { createStyles, Navbar, Group, Code, getStylesRef, rem } from '@mantine/core';
+import {
+  IconBellRinging,
+  IconFingerprint,
+  IconKey,
+  IconSettings,
+  Icon2fa,
+  IconDatabaseImport,
+  IconReceipt2,
+  IconSwitchHorizontal,
+  IconLogout,
+  IconHome,
+  IconSubscript,
+  IconDatabaseExport,
+  IconPhoto,
+  IconUserCircle,
+} from '@tabler/icons-react';
+import { MantineLogo } from '@mantine/ds';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
-
-function App() {
-
-const views =[
-{
-  path:'/home',
-  name: 'Home',
-  component : null
-},
-{
-  path:'/settings',
-  name: 'Settings',
-  component : null
-},
-{
-  path:'/login',
-  name: 'Logout',
-  component : null
-},]
-
-const [opened, setOpened] = useState(false);
-const defColorScheme = 'dark';
-const [colorScheme, setColorScheme] = useState(defColorScheme);
-
-const toggleColorScheme = value =>{
-  const newValue = value || (colorScheme === 'dark' ? 'light' : 'dark');
-  setColorScheme(newValue);
-}
 
 const useStyles = createStyles((theme) => ({
-  navLink: {
-    display: 'block',
-    width: '100%',
-    padding: theme.spacing.xs,
-    borderRadius: theme.spacing.sm,
-    color: colorScheme ==='dark' ? theme.colors.dark[0]:theme.black,
-    textDecoration: 'none',
-
-    '&:hover' : {
-      backgroundColor: colorScheme ==='dark' ? theme.colors.dark[6]:theme.colors.gray[1],
-    }
+  header: {
+    paddingBottom: theme.spacing.md,
+    marginBottom: `calc(${theme.spacing.md} * 1.5)`,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
   },
-  navLinkActive:{
-    backgroundColor: colorScheme ==='dark' ? theme.colors.dark[6]:theme.colors.gray[1],
-  }
+
+  footer: {
+    paddingTop: theme.spacing.md,
+    marginTop: theme.spacing.md,
+    borderTop: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+  },
+
+  link: {
+    ...theme.fn.focusStyles(),
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    fontSize: theme.fontSizes.sm,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[7],
+    padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      },
+    },
+  },
+
+  linkIcon: {
+    ref: getStylesRef('icon'),
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[6],
+    marginRight: theme.spacing.sm,
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      [`& .${getStylesRef('icon')}`]: {
+        color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+      },
+    },
+  },
 }));
 
-const {classes} = useStyles();
+const data = [
+  { link: '', label: 'Home', icon: IconHome },
+  { link: '', label: 'Notifications', icon: IconBellRinging },
+  { link: '', label: 'Profile', icon: IconUserCircle  },
+  { link: '', label: 'Subscription', icon: IconReceipt2},
+  { link: '', label: 'Dataset', icon: IconDatabaseExport },
+  { link: '', label: 'Settings', icon: IconSettings },
+];
 
-  return (
-    <MantineProvider theme = {{colorScheme: colorScheme, fontFamily:'Exo 2, sans-serif'}} with GlobalStyles>
-      <MemoryRouter>
-        <AppShell padding="md" navbarOffsetBreakpoint="sm" fixed navbar={
-          <Navbar width={{sm:200}} padding="xs" hidden={!opened} hiddenBreakpoint="sm">
-            {
-              views.map((view,index) => 
-              <NavLink align="left" to={view.path} key={index} onClick={() =>setOpened(false)} className={({ isActive }) => classes.navLink + ' ' +  (isActive ? classes.navLinkActive: '')}>
-                {
-                  //Icons go here
-                }
-                <Group>
-                  <Text>
-                    {view.name}
-                  </Text></Group>
-              </NavLink>
-              )
-            }
-          </Navbar>
-        }
-        
-        header = {
-          <Header height ={70} padding = "md">
-            <div style = {{display : 'flex', alignItems : 'center', height:'100%'}}>
-
-          <MediaQuery largerThan="sm" styles = {{display: 'none'}}>
-            <Burger opened ={opened} onClick = {() => setOpened((o) => !o)} size ="sm" color ={useMantineTheme().colors.gray[6]} mr="x1" />
-          </MediaQuery>
-          <Text> EZHire </Text>
-          <div style ={{marginLeft: "auto"}}>
-            <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
-              {colorScheme === 'dark' ? <SunIcon /> : <MoonIcon/>}
-            </ActionIcon>
-            </div>
-         
-            </div>
-          </Header>
-        }
-        styles = {theme => ({
-          main : {backgroundColor : theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.dark[0]},
-        })}
-        >
-          <Routes>
-            {
-            views.map((view, index) => <Route key={index} path ={view.path} element = {<view.component />} />)
-}
-          </Routes>
-          </AppShell>
-        </MemoryRouter>
-        </MantineProvider>
-  );
-}
-
+function App() {
+  
+    return (
+      <div className={'App'}>
+    <BrowserRouter>
+<Routes>
+<Route path="/Register" element={<Register />} />
+<Route path="/login" element={<Login />}/>
+<Route path="/Home" element={<Home />} />
+navigate(path);
+   </Routes>
+</BrowserRouter>
+      </div>
+    );
+  }
 export default App;
