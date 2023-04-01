7544ce53a7a9eca586248b123e17fd7bed99152b
Nabeel Mirza <77447920+SBSixteen@users.noreply.github.com>
2023-04-01 15:45:31 +0500

    I fucked up

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,17 +1,88 @@
 import { useState } from "react";
+import React from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import "./style.css";
+// import { Button } from "./Components/Button";
+import Button from "./Components/Button/";
 
-function Register(){
+function Register() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [emailID, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [Remember, setRememberMe] = useState(false);
 
-    return(
+  return (
+    <div className="container">
+      <div className="row">
+        <a href="" target="_blank">
+          <img src="/Logo_Ezhire.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+      </div>
 
-        <div className="container">
+      <h1>Registeration</h1>
 
-        <p> Register page   </p>
+      <div
+        className="column"
+        style={{
+          display: "flex",
+          flex: 1,
+          alignItems: "center",
+        }}
+      >
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            className="default_gap"
+            id="username-input"
+            onChange={(e) => setEmail(e.currentTarget.value)}
+            placeholder="Email ID"
+          />
 
-        </div>
-    );
+          <br></br>
 
+          <input
+            className="default_gap"
+            id="username-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter Username"
+          />
+
+          <br></br>
+
+          <input
+            className="default_gap"
+            type="Password"
+            id="password-input"
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            placeholder="Password"
+          />
+
+          <br></br>
+
+          <input
+            className="default_gap"
+            type="Confirm Password"
+            id="password-input"
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            placeholder="Confirm Password"
+          />
+
+          <br></br>
+
+          <Button title="Register" />
+
+          <br></br>
+        </form>
+      </div>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
 }
-export default Register
+
+export default Register;
